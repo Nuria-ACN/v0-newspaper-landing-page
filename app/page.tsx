cd54e19e2cd104d8419f30692421d63a8ef55919
Nuria-ACN <nuria.salom.soriano@accenture.com>
2026-03-08 13:36:18 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Portada</h1>
+      <p>Si ves esto, ya funciona la ruta /.</p>
+    </main>
+  );
+}
